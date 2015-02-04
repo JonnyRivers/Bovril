1e9ca41360113bd05740eba04b3c788858cb9765
JonnyRivers <jonny.w.rivers@gmail.com>
2015-02-03 22:18:46 -0800

    Added UrlsAndRoutes project

---
+++ ProASP.NETMVC5/UrlsAndRoutes.Tests/RouteTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Reflection;
+using System.Web;
+using System.Web.Routing;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Moq;
+
+namespace UrlsAndRoutes.Tests
+{
+    [TestClass]
+    public class RouteTests
+    {
+        private HttpContextBase CreateHttpContext(String targetUrl = null, String httpMethod = "GET")
+        {
+            // create the mock request
+            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
+            mockRequest.Setup(m => m.AppRelativeCurrentExecutionFilePath)
+                .Returns(targetUrl);
+
+            // Create the mock response
+            Mock<HttpResponseBase> mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(m => m.ApplyAppPathModifier(It.IsAny<String>())).Returns<String>(s => s);// ApplyAppPathModifier(s) => s
+            
+            // Create the mock context from the mock request and mock response
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(m => m.Request).Returns(mockRequest.Object);
+            mockContext.Setup(m => m.Response).Returns(mockResponse.Object);
+
+            return mockContext.Object;
+        }
+
+        private bool TestIncomingRouteResult(
+            RouteData routeResult, 
+            String controller, 
+            String action, 
+            Object propertySet = null)
+        {
+            Func<object, object, bool> valCompare = (v1, v2) =>
+            {
+                return StringComparer.InvariantCultureIgnoreCase.Compare(v1, v2) == 0;
+            };
+
+            bool result = valCompare(routeResult.Values["controller"], controller)
+                && valCompare(routeResult.Values["action"], action);
+
+            if (propertySet != null)
+            {
+                PropertyInfo[] propInfo = propertySet.GetType().GetProperties();
+                foreach (PropertyInfo pi in propInfo)
+                {
+                    if (!(routeResult.Values.ContainsKey(pi.Name)
+                        && valCompare(routeResult.Values[pi.Name], pi.GetValue(propertySet, null))))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void TestRouteMatch(
+            String url,
+            String controller,
+            String action,
+            Object routeProperties = null,
+            String httpMethod = "GET")
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            // Act
+            RouteData result = routes.GetRouteData(CreateHttpContext(url, httpMethod));
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(TestIncomingRouteResult(result, controller, action, routeProperties));
+        }
+
+        private void TestRouteFail(String url)
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            // Act
+            RouteData result = routes.GetRouteData(CreateHttpContext(url));
+
+            // Assert
+            Assert.IsTrue(result == null || result.Route == null);
+        }
+
+        [TestMethod]
+        public void TestRouteMatch()
+        {
+            // check for a URL that is covered in the app
+            TestRouteMatch("~/Admin/Index", "Admin", "Index");
+
+            // check for a URL that isn't covered in the app, but still matches
+            TestRouteMatch("~/One/Two", "One", "Two");
+
+            // check that too many arguments fails
+            TestRouteFail("~/Admin/Index/Segment");
+
+            // check that too few arguments fails
+            TestRouteFail("~/Admin");
+        }
+    }
+}
