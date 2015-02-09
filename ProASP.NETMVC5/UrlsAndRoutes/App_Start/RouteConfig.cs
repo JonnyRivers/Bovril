7ea41665110a91174249b338a6a66187ae8e155c
JonnyRivers <jonny.w.rivers@gmail.com>
2015-02-08 16:43:35 -0800

    Completed Chapter 16

--- ProASP.NETMVC5/UrlsAndRoutes/App_Start/RouteConfig.cs
+++ ProASP.NETMVC5/UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -14,45 +14,18 @@ namespace UrlsAndRoutes
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            // This demonstrates the ability to define your own constraints.
-            // In this example we are detecting the client's browser and routing
-            // them accordingly.
-            // See UrlsAndRoutes.Infrastructure.UserAgentConstraint for impl details.
-
-            //routes.MapRoute(
-            //    "ChromeRoute",
-            //    "{*catchall}",
-            //    new
-            //    {
-            //        controller = "Home",
-            //        action = "Chrome"
-            //    },
-            //    new { customConstraint = new UserAgentConstraint("Chrome") });
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
-                "MyRoute",                                          // route name
-                "{controller}/{action}/{id}/{*catchall}",           // url
-                new                                                 // default values for url segments
-                {
+                "MyRoute",
+                "{controller}/{action}/{id}",
+                new{
                     controller = "Home",
                     action = "Index",
                     id = UrlParameter.Optional
                 },
-                new                                                 // constraints
-                {
-                    controller = "^H.*",                            // controller must begin(^) with H
-                    action = "^Index$|^About$",                     // action must be either Index or About
-                    //id = new RangeRouteConstraint(10, 20)         // put a range on the id
-                    //id = new CompoundRouteConstraint(             // make a compound constraint
-                    //    new IRouteConstraint[] { 
-                    //        new AlphaRouteConstraint(),
-                    //        new MinLengthRouteConstraint(6)})
-                    //httpMethod = new HttpMethodConstraint("GET")  // THIS KILLS THE TESTS!
-                },
-                new[]                                              // namespaces to look in
-                {
-                    "UrlsAndRoutes.Controllers"
-                });
+                null,
+                new[] { "UrlsAndRoutes.Controllers" });// this prevents the admin area mixing our shit up
         }
     }
 }
