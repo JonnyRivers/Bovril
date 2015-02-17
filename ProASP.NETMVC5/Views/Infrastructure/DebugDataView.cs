d490348d9b639cf1c31d90332b62d56082cb5299
JonnyRivers <jonny.w.rivers@gmail.com>
2015-02-16 20:01:14 -0800

    Start of Chapter 20

---
+++ ProASP.NETMVC5/Views/Infrastructure/DebugDataView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Views.Infrastructure
+{
+    public class DebugDataView : IView
+    {
+        public void Render(ViewContext viewContext, TextWriter writer)
+        {
+            Write(writer, "---Routing Data---");
+            foreach (String key in viewContext.RouteData.Values.Keys)
+            {
+                Write(
+                    writer, 
+                    "Key: {0}, Value: {1}",
+                    key,
+                    viewContext.RouteData.Values[key]);
+            }
+
+            Write(writer, "---View Data---");
+            foreach (String key in viewContext.ViewData.Keys)
+            {
+                Write(
+                    writer,
+                    "Key: {0}, Value: {1}",
+                    key,
+                    viewContext.ViewData[key]);
+            }
+        }
+
+        private void Write(TextWriter writer, String template, params Object[] values)
+        {
+            writer.Write(String.Format(template, values) + "<p/>");
+        }
+    }
+}
---
+++ ProASP.NETMVC5/Views/Infrastructure/DebugDataViewEngine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Views.Infrastructure
+{
+    public class DebugDataViewEngine : IViewEngine
+    {
+        public ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            return new ViewEngineResult(
+                new String[] { 
+                    "No view (Debug Data View Engine)"
+                }
+            );
+        }
+
+        public ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            if (viewName == "DebugData")
+            {
+                return new ViewEngineResult(new DebugDataView(), this);
+            }
+            else
+            {
+                return new ViewEngineResult(
+                    new String[] { 
+                        // this is supposed to be 'where the IViewEngine implementation looked'
+                        "No view (Debug Data View Engine)"
+                    }
+                );
+            }
+        }
+
+        public void ReleaseView(ControllerContext controllerContext, IView view)
+        {
+            // do nowt
+        }
+    }
+}
