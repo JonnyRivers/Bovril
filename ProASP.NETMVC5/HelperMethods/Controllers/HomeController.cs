5cc6d2da39e2698ca8a059e3d0d3d6166b838d6c
JonnyRivers <jonny.w.rivers@gmail.com>
2015-03-01 14:29:00 -0800

    Chapter 21 done

---
+++ ProASP.NETMVC5/HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using HelperMethods.Models;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new String[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new String[] { "New York", "London", "Paris" };
+
+            String message = "This is an HTML element: <input>";
+
+            return View((Object)message);
+        }
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person person)
+        {
+            return View(person);
+        }
+    }
+}
---
+++ ProASP.NETMVC5/HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Infrastructure
+{
+    public static class CustomHelpers
+    {
+        // This is the C# equivalent of the following inline helper.
+        // It's imperative nature is harder to parse and less 'html'.
+        //
+        // @helper ListArrayItems(String[] items) { 
+        //     <ul>
+        //         @foreach (string str in items) {
+        //             <li>@str</li>
+        //         }
+        //     </ul>
+        // }
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, String[] list)
+        {
+            TagBuilder listTagBuilder = new TagBuilder("ul");
+
+            foreach (String str in list)
+            {
+                TagBuilder listItemTagBuilder = new TagBuilder("li");
+                listItemTagBuilder.SetInnerText(str);
+                listTagBuilder.InnerHtml += listItemTagBuilder.ToString();
+            }
+
+            return new MvcHtmlString(listTagBuilder.ToString());
+        }
+
+        public static MvcHtmlString DisplayMessage(this HtmlHelper htmlHelper, String message)
+        {
+            // HtmlHelper.Encode() is required to prevent form elements from being rendered verbatim.
+            // e.g. 
+            String encodedMessage = htmlHelper.Encode(message);
+            String result = String.Format("This is the message: <p>{0}</p>", encodedMessage);
+
+            return new MvcHtmlString(result);
+        }
+    }
+}
