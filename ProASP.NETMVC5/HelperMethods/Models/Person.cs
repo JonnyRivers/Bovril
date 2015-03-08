f49e665c68db1bd03f71064247b422a2990cb189
JonnyRivers <jonny.w.rivers@gmail.com>
2015-03-07 16:56:34 -0800

    Chapter 22 done

--- ProASP.NETMVC5/HelperMethods/Models/Person.cs
+++ ProASP.NETMVC5/HelperMethods/Models/Person.cs
@@ -1,18 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HelperMethods.Models
 {
-    public class Person
+    // rather than clutter this core type with lots of Mvc specific annotations, we define those elsewhere
+    [MetadataType(typeof(PersonMetaData))]
+    public partial class Person
     {
         public int PersonId { get; set; }
+
         public String FirstName { get; set; }
+
         public String LastName { get; set; }
+
         public DateTime BirthDate { get; set; }
+
         public Address HomeAddress { get; set; }
+
         public bool IsApproved { get; set; }
+
         public Role Role { get; set; }
     }
 }
---
+++ ProASP.NETMVC5/HelperMethods/Models/PersonMetaData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace HelperMethods.Models
+{
+    [DisplayName("New Person")]
+    public partial class PersonMetaData
+    {
+        // This HiddenInput property prevents the property from being drawn
+        [HiddenInput(DisplayValue = false)]
+        public int PersonId { get; set; }
+
+        [Display(Name = "First")]
+        public String FirstName { get; set; }
+
+        [Display(Name = "Last")]
+        public String LastName { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Approved")]
+        public bool IsApproved { get; set; }
+    }
+}
