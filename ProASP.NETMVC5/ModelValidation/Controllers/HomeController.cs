ba9da099ac12104750ac9029729e7128a4de4af1
JonnyRivers <jonny.w.rivers@gmail.com>
2015-03-09 23:12:09 -0700

    Chapter 25 started

---
+++ ProASP.NETMVC5/ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ModelValidation.Models;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appointment)
+        {
+            // TODO: store the appointment
+
+            return View("Completed", appointment);
+        }
+    }
+}
