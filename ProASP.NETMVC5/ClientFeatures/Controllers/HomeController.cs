ede30acdeb3f6d0e762b0c066acf038fc65b855a
JonnyRivers <jonny.w.rivers@gmail.com>
2015-03-14 12:59:58 -0700

    Chapter 26 done

---
+++ ProASP.NETMVC5/ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using ClientFeatures.Models;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult MakeBooking()
+        {
+            Appointment appointment = new Appointment
+            {
+                ClientName = "Adam",
+                TermsAccepted = true
+            };
+            return View(appointment);
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appointment)
+        {
+            return Json(appointment, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ProASP.NETMVC5/ClientFeatures/Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClientFeatures.Models
+{
+    public class Appointment
+    {
+        [Required]
+        public String ClientName { get; set; }
+
+        public bool TermsAccepted { get; set; }
+    }
+}
