555483df3a7666bf6a31cba10bdfc20704dcc11a
JonnyRivers <jonny.w.rivers@gmail.com>
2015-03-14 16:18:11 -0700

    Chapter 27 done

--- ProASP.NETMVC5/WebServices/Controllers/HomeController.cs
+++ ProASP.NETMVC5/WebServices/Controllers/HomeController.cs
@@ -12,36 +12,7 @@ namespace WebServices.Controllers
     {
         public ActionResult Index()
         {
-            return View(ReservationRepository.Current.GetAll());
-        }
-
-        public ActionResult Add(Reservation item)
-        {
-            if (ModelState.IsValid)
-            {
-                ReservationRepository.Current.Add(item);
-
-                return RedirectToAction("Index");
-            }
-
-            return View("Index");
-        }
-
-        public ActionResult Remove(int id)
-        {
-            ReservationRepository.Current.Remove(id);
-
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Update(Reservation item)
-        {
-            if (ModelState.IsValid && ReservationRepository.Current.Update(item))
-            {
-                return RedirectToAction("Index");
-            }
-
-            return View("Index");
+            return View();
         }
     }
 }
---
+++ ProASP.NETMVC5/WebServices/Controllers/WebController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class WebController : ApiController
+    {
+        public IEnumerable<Reservation> GetAllReservations()
+        {
+            return ReservationRepository.Current.GetAll();
+        }
+
+        public Reservation GetReservation(int id)
+        {
+            return ReservationRepository.Current.Get(id);
+        }
+
+        //public Reservation PostReservation(Reservation item)
+        [HttpPost]
+        public Reservation CreateReservation(Reservation item)
+        {
+            return ReservationRepository.Current.Add(item);
+        }
+
+        //public bool PutReservation(Reservation item)
+        [HttpPut]
+        public bool UpdateReservation(Reservation item)
+        {
+            return ReservationRepository.Current.Update(item);
+        }
+
+        public void DeleteReservation(int id)
+        {
+            ReservationRepository.Current.Remove(id);
+        }
+    }
+}
